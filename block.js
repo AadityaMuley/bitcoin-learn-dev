270a13a2daa7aa0fc28b87e5f4bfecf02169754d
AadityaMuley <aadityamuley8190@gmail.com>
2021-03-11 13:44:28 +0530

    SHA-256 Hashing function added to generate unique hashes and script added

--- block.js
+++ block.js
@@ -1,29 +1,39 @@
+const SHA256 = require("crypto-js/sha256"); //accessing the SHA-256 func of crypto-js
+
 class Block {
-    constructor(timestamp, lastBlockHash, currentBlockHash, data) {
+    constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
-        this.lastBlockHash = lastBlockHash;
-        this.currentBlockHash = currentBlockHash;
+        this.lastHash = lastHash; //last block hash
+        this.hash = hash; //current block hash
         this.data = data;
     }
 
+    //to ease printing block data
     toString() {
         return `Block -
             Timestamp: ${this.timestamp}
-            Last Block Hash: ${this.lastBlockHash.substring(0, 10)}
-            Current Block Hash: ${this.currentBlockHash.substring(0, 10)}
-            Data: ${this.data}`;
+            Last Block Hash: ${this.lastHash.substring(0, 10)}
+            Current Block Hash: ${this.hash.substring(0, 10)}
+            Data: ${this.data}`; //notice we are not return normal string but combining all these into one string using ES6 template string
     }
 
+    //genesis block
     static genesis() {
         return new this("Genesis Time", "----------", "f1r57-h45h", []);
     }
 
+    //mining block
     static mineBlock(lastBlock, data) {
         const timestamp = Date.now();
-        const lastHash = lastBlock.currentBlockHash;
-        const hash = "todo-hash";
+        const lastHash = lastBlock.hash;
+        const hash = Block.hash(timestamp, lastHash, data);
 
         return new this(timestamp, lastHash, hash, data);
+    }
+
+    //static hash func to generate unique hash
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString(); //using es6 template string to combine all inputs since we need to hash all inputs to get the unique hash
     }
 }
 
