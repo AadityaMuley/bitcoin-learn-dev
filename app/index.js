96aceada3e765a06ac2ae63c64bdafa1f512ddf7
AadityaMuley <aadityamuley8190@gmail.com>
2021-03-15 21:30:04 +0530

    Application created using npm express module, API created (accessible through Postman) and npm body-parser module added to get JSON data from POST request to add new blocks

---
+++ app/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Blockchain = require("../blockchain");
+
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+//process.env.HTTP_PORT is used to get port number from the user instead of default 3001
+//this is required when we want to run multiple instances of the same app so every instance must run on a different port
+//terminal command to specify port number:
+// $ HTTP_PORT=3002 npm run dev
+
+const app = express();
+const bc = new Blockchain();
+
+app.use(bodyParser.json());
+
+//GET request which returens all blocks to display all current blocks to user - /blocks is the endpoint we want the API to expose
+app.get("/blocks", (request, response) => {
+    response.json(bc.chain);
+});
+
+//POST request to add a new block and then display the Blockchain with the newly added block
+app.post("/mine", (request, response) => {
+    const block = bc.addBlock(request.body.data);
+    console.log(`New block added: ${block.toString()}`);
+
+    response.redirect("/blocks");
+});
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
