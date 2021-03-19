ca8608489e682efc5409ae46e2e5eb45a5caa2a3
AadityaMuley <aadityamuley8190@gmail.com>
2021-03-19 22:00:10 +0530

    tests added in block.test.js to test if difficulty if adjusted dynamically and a new development test added in dev-test.js to check working for 10 blocks

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,5 +1,5 @@
 const Block = require("./block");
-const { DIFFICULTY } = require("../config");
+//const { DIFFICULTY } = require("../config");
 
 describe("Block", () => {
     let data, lastBlock, block;
@@ -20,7 +20,15 @@ describe("Block", () => {
     });
 
     it("generates a hash that matches the DIFFICULTY", () => {
-        expect(block.hash.substring(0, DIFFICULTY)).toEqual("0".repeat(DIFFICULTY));
+        expect(block.hash.substring(0, block.difficulty)).toEqual("0".repeat(block.difficulty));
         console.log(block.toString());
+    });
+
+    it("lowers the difficulty for slowly mined blocks", () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+360000)).toEqual(block.difficulty-1);
+    });
+
+    it("raises the difficulty for quickly mined blocks", () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+1)).toEqual(block.difficulty+1);
     });
 });
--- dev-test.js
+++ dev-test.js
@@ -1,8 +1,16 @@
-const Block = require("./block");
+// const Block = require("./block");
 
-// const block = new Block("timestamp", "last hash", "current hash", "data");
-// console.log(block.toString());
-// console.log(Block.genesis().toString());
+// // const block = new Block("timestamp", "last hash", "current hash", "data");
+// // console.log(block.toString());
+// // console.log(Block.genesis().toString());
 
-const firstBlock = Block.mineBlock(Block.genesis(), "first");
-console.log(firstBlock.toString());
+// const firstBlock = Block.mineBlock(Block.genesis(), "first");
+// console.log(firstBlock.toString());
+
+const Blockchain = require("./blockchain");
+
+const bc = new Blockchain;
+
+for (let i=0; i<10; i++) {
+    console.log(bc.addBlock(`Transaction ${i}`).toString());
+}
