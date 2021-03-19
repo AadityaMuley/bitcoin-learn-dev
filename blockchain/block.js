21dc0e559922513ffd354ad798fa84421981a068
AadityaMuley <aadityamuley8190@gmail.com>
2021-03-19 21:46:53 +0530

    Mining rate variable added. Dynamically adjusting the difficulty of mine func using the timestamps

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,13 +1,14 @@
 const SHA256 = require("crypto-js/sha256"); //accessing the SHA-256 func of crypto-js
-const { DIFFICULTY } = require("../config"); //fetches the DIFFICULTY const from config.js file
+const { DIFFICULTY, MINE_RATE } = require("../config"); //fetches the DIFFICULTY const from config.js file
 
 class Block {
-    constructor(timestamp, lastHash, hash, data, nonce) {
+    constructor(timestamp, lastHash, hash, data, nonce, difficulty) {
         this.timestamp = timestamp;
         this.lastHash = lastHash; //last block hash
         this.hash = hash; //current block hash
         this.data = data;
         this.nonce = nonce;
+        this.difficulty = difficulty || DIFFICULTY;
     }
 
     //to ease printing block data
@@ -17,36 +18,45 @@ class Block {
             Last Block Hash: ${this.lastHash.substring(0, 10)}
             Current Block Hash: ${this.hash.substring(0, 10)}
             Nonce : ${this.nonce};
+            Difficulty: ${this.difficulty};
             Data: ${this.data}`; //notice we are not return normal string but combining all these into one string using ES6 template string
     }
 
     //genesis block
     static genesis() {
-        return new this("Genesis Time", "----------", "f1r57-h45h", [], 0); // 0 - default nonce for genesis block
+        return new this("Genesis Time", "----------", "f1r57-h45h", [], 0, DIFFICULTY); // 0 - default nonce for genesis block
     }
 
     //mining block
     static mineBlock(lastBlock, data) {
         let hash, timestamp;
         const lastHash = lastBlock.hash;
+        let { difficulty } = lastBlock;
         let nonce = 0;
         do{
             nonce++;
             timestamp = Date.now();
-            hash = Block.hash(timestamp, lastHash, data, nonce);
-        } while(hash.substring(0, DIFFICULTY) !== "0".repeat(DIFFICULTY));
+            difficulty = Block.adjustDifficulty(lastBlock, timestamp);
+            hash = Block.hash(timestamp, lastHash, data, nonce, difficulty);
+        } while(hash.substring(0, difficulty) !== "0".repeat(difficulty));
 
-        return new this(timestamp, lastHash, hash, data, nonce);
+        return new this(timestamp, lastHash, hash, data, nonce, difficulty);
     }
 
     //static hash func to generate unique hash
-    static hash(timestamp, lastHash, data, nonce) {
-        return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString(); //using es6 template string to combine all inputs since we need to hash all inputs to get the unique hash
+    static hash(timestamp, lastHash, data, nonce, difficulty) {
+        return SHA256(`${timestamp}${lastHash}${data}${nonce}${difficulty}`).toString(); //using es6 template string to combine all inputs since we need to hash all inputs to get the unique hash
     }
 
     static blockHash(block) {
-        const {timestamp, lastHash, data, nonce} = block;
-        return Block.hash(timestamp, lastHash, data, nonce);
+        const {timestamp, lastHash, data, nonce, difficulty} = block;
+        return Block.hash(timestamp, lastHash, data, nonce, difficulty);
+    }
+
+    static adjustDifficulty(lastBlock, currentTimestamp) {
+        let { difficulty } = lastBlock;
+        difficulty = lastBlock.timestamp+MINE_RATE > currentTimestamp ? difficulty+1 : difficulty-1
+        return difficulty;
     }
 }
 
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const DIFFICULTY = 3; //difficulty of calculating nonce value
+const MINE_RATE = 3000; //in milliseconds
 
-module.exports = { DIFFICULTY };
+module.exports = { DIFFICULTY, MINE_RATE };
